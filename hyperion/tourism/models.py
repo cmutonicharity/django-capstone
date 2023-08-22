ddbe0e0db2aa55484abbf233474be3d03b83f2c9
Charity Mutoni <charity_91@outlook.com>
2023-08-22 22:22:13 +0100

    Docstring for Booking Model

--- hyperion/tourism/models.py
+++ hyperion/tourism/models.py
@@ -28,10 +28,28 @@ class Activity(models.Model):
 
 
 class Booking(models.Model):
+    """
+    Booking Model represents a Booking made of an `activity` by a `user`.
+
+    Attributes
+    ----------
+    activity : Activity
+        The activity booked by the `user`
+    user : User
+        The user who made the booking
+    number_of_people : IntegerField
+        The number of people booked for
+    """
     activity = models.ForeignKey(Activity, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     number_of_people = models.IntegerField()
 
     def total(self):
+        """
+        Gets the total cost of the booking
+
+        The total cost is based on the `activity.price` and `number_of_people`
+        :return: the total price of the booking
+        """
         unit_cost = self.activity.price
         return unit_cost * self.number_of_people
