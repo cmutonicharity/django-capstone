54f36bc8e37cfccd9c60d22d3006349e7183d1a6
Charity Mutoni <charity_91@outlook.com>
2023-08-23 00:58:33 +0100

    Removed secret. Must now use a new one

--- hyperion/hyperion/settings.py
+++ hyperion/hyperion/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+t37p=t^7i-#^ait_k#i6!=k&qpsjq8d&g23x@7)l!x(wju$p+'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
