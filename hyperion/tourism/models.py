59818740f09af9e44d5f1b46fa885de4a33953f7
Charity Mutoni <charity_91@outlook.com>
2023-08-22 22:18:59 +0100

    Docstring for Activity Model

--- hyperion/tourism/models.py
+++ hyperion/tourism/models.py
@@ -4,6 +4,20 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Activity(models.Model):
+    """
+    Activity Model represents an activity that can be booked.
+
+    Attributes
+    ----------
+    title : CharField
+        the short title describing the activity, limited to 200 characters.
+    description : TextField
+        the full description of the activity.
+    price : DecimalField
+        the per person price for the activity.
+    image : CharField
+        the filename of the image to be displayed. Images are stored in static/tour-uploads
+    """
     title = models.CharField(max_length=200)
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=6)
